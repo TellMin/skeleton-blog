c7bb6faedd1356c05b642850a4aadcb842b84ba4
TellMin <dhgirkcls@gmail.com>
2023-03-25 20:53:12 +0900

    Add @iconify/svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<h1>Let's get cracking bones!</h1>
+	<Icon icon="carbon:logo-github" class="text-secondary-900 text-3xl" />
 </div>
